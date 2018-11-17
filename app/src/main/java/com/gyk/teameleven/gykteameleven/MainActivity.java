2a2a669bfc7c57c935d07c3561d514a20ab3122a
dcanaktemur <doguscanaktemur@gmail.com>
2018-11-17 16:30:32 +0300

    initial commit

---
+++ app/src/main/java/com/gyk/teameleven/gykteameleven/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gyk.teameleven.gykteameleven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
