eca0b5814da98d7fbb56c5862ecf25a5f8b88616
mustafa <mustafaarifsisman@gmail.com>
2018-11-18 18:45:45 +0300

    Merge branch 'master' of https://github.com/dcanaktemur/gyk-team-eleven
    # Conflicts:
    #	app/src/main/java/com/gyk/teameleven/gykteameleven/DetailActivity.java

--- app/src/main/java/com/gyk/teameleven/gykteameleven/DetailActivity.java
+++ app/src/main/java/com/gyk/teameleven/gykteameleven/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.gyk.teameleven.gykteameleven;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -19,6 +23,9 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView name, email, description, favoriteTopics, comments;
         RatingBar stars;
+
+        ImageButton callButton;
+        int status;
         ImageView photo;
 
         //Views
@@ -28,7 +35,8 @@ public class DetailActivity extends AppCompatActivity {
         favoriteTopics = findViewById(R.id.favoriteTopics);
         comments = findViewById(R.id.comments);
         photo = findViewById(R.id.avatar);
-        stars = findViewById(R.id.ratingBar);
+        stars = (RatingBar) findViewById(R.id.ratingBar);
+        callButton = (ImageButton) findViewById(R.id.callButton);
 
         //Setters
         name.setText(teacher.getName() + " " + teacher.getSurname());
@@ -40,6 +48,12 @@ public class DetailActivity extends AppCompatActivity {
                 .load(teacher.getPhotoUrl())
                 .into(photo);
         stars.setRating(teacher.getStars());
-
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, CallActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
